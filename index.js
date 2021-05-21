1f84031849549225223df10ed2a327cce02d56fb
adrian154 <94568a@gmail.com>
2021-05-21 09:10:35 -0700

    add bot functionality

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+const fs = require("fs");
 const sharp = require("sharp");
 const MC = require("node-mc-api");
-const fetch = require("node-fetch");
 const Express = require("express");
+const fetch = require("node-fetch");
 const Discord = require("discord.js");
 
-// local deps
+// ----- local deps
 const MCWS = require("./mcws.js");
 const config = require("./config.json");
 
+// ----- utility funcs
+const saveConfig = () => {
+    fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
+};
+
+const ignore = () => {};
+
+// ----- misc state
 const profileCache = {};
 
+// ----- init webend
 const express = Express();
 express.get("/profile", (req, res) => {
     const profile = profileCache[req.query.uuid];
@@ -24,13 +34,19 @@ express.get("/profile", (req, res) => {
 
 express.listen(config.port, () => console.log(`Webend started listening on port ${config.port}`));
 
+// ----- init bot/discord link
 const mcServer = new MCWS(config.mcws.host);
+const bot = new Discord.Client();
+
 mcServer.waitConnect().then(() => {
     mcServer.auth(config.mcws.clientID, config.mcws.secret).then(() => {
-        console.log("Authed successfully");5
+        console.log("Authed successfully");
     }).catch(console.error);
 });
 
+bot.login(config.discord.token);
+
+// ----- set up events for mc server
 mcServer.on("chat", async (event) => { 
     
     // fetch profile
@@ -58,5 +74,48 @@ mcServer.on("chat", async (event) => {
 
 });
 
-const bot = new Discord.Client();
-bot.login(config.discord.token);
+mcServer.on("console", (event) => {
+    if(config.consoleChannel && bot.ready) {
+        const channel = bot.channels.cache.get(config.consoleChannel);
+        if(channel) {
+            channel.send("`" + event.message + "`");
+        }
+    }
+});
+
+// ----- set up bot logic
+bot.on("ready", () => {
+    console.log(`Logged in as ${bot.user.tag}`);
+    bot.ready = true;
+});
+
+bot.on("message", (message) => {
+    
+    if(message.content[0] === "-") {
+        
+        const tokens = message.content.trim().split(/\s+/);
+        const command = tokens[0].slice(1, tokens[0].length);
+        
+        if(command === "bindchannel") {
+            if(tokens[1] === "mc") {
+                config.mcChannel = message.channel.id;
+                saveConfig();
+            } else if(tokens[1] === "console") {
+                config.consoleChannel = message.channel.id;
+                saveConfig();
+            } else {
+                message.channel.send("Incorrect arguments. Usage: `-bindchannel mc/console`").catch(ignore);
+            }
+        }
+
+    } else {
+        if(message.channel.id === config.mcChannel) {
+            // TODO: deserialize discord format
+            const jsonMessage = {text: `[Discord] ${message.author.username}: ${message.content}`};
+            mcServer.runCommand(`tellraw @a ${JSON.stringify(jsonMessage)}`);
+        } else if(message.channel.id === config.consoleChannel) {
+            mcServer.runCommand(message.content);
+        }
+    }
+
+});
